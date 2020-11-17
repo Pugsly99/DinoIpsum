59e938fc37932295eea27fb570d3a41912fb86b5
Pugsly99 <rylandadams@yahoo.com>
2020-11-17 15:59:03 -0800

    letter check functonality
    Co-Authored-by: Harrison Strand <harrisonstrand@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,31 +6,48 @@ import DinoIpsum from './services/dino-ipsum';
 
 //BL
 function clearFields() {
-  $('#showLetters').text("");
+  $('#row').text("");
   $('#letterGuess').val("");
 }
 
 function displayDinoWord(dinoArray) {
   for(let j = 0; j <= dinoArray.length; j++) {
     $(`#letter${j}`).text(dinoArray[j]);
+    $(`#underscore${j}`).text("_ ");
   }
-  
 }
+
+function checkLetter(letter, dinoArray) {
+  let wrong = 0;
+  for(let l = 0; l <= dinoArray.length; l++) {
+    if(letter === dinoArray[l]) {
+      $(`#letter${l}`).show();
+      $(`#underscore${l}`).hide();
+    } else if(is_dinoArray(letter) = false) {
+      console.log(wrong);
+      wrong = wrong + 1
+    } else if(wrong === 8) {
+      alert("OUT OF TURNS");
+    }
+  }
+}
+
 function displayErrors(error) {
-  $('#showLetters').text(`${error}`);
+  $('#errors').text(`${error}`);
 }
 
 //UL
 $(document).ready(function() {
-  $('#dinoButton').click(function() {
+  let dinoArray = [];
+  $('#dinoButton').click(function(event) {
     clearFields();
+    event.preventDefault();
     DinoIpsum.getIpsum()
       .then(function(dinoResponse) {
         if (dinoResponse instanceof Error) {
           throw Error(`Dino API error: ${dinoResponse.message}`);
         }
         const dinoWord = dinoResponse.toString().split("");
-        let dinoArray = [];
         for(let i = 0; i <= dinoWord.length; i++) {
           dinoArray.push(dinoWord[i]);
         }
@@ -41,7 +58,15 @@ $(document).ready(function() {
       .catch(function(error) {
         displayErrors(error.message);
       });
+
+    $('#letterButton').click(function(event) {
+      event.preventDefault();
+      let letter = $('#letterGuess').val();
+      checkLetter(letter, dinoArray);
+    });
   });
+
+  
 });
 
 
